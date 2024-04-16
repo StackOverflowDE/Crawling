4b56b593357151793c7886ba2c75090e4c89c3f9
ryd11 <sdmee97@gmail.com>
2024-04-16 11:58:07 +0900

    github크롤링
    github크롤링 해보았는데 watch가 안되네요 혹시 해보시고 되면 알려주세요!

---
+++ githubcrawling.py
@@ -0,0 +1,59 @@
+user_agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+
+import requests
+from bs4 import BeautifulSoup
+
+'''
+language와 sort_tag를 정보에 맞게 넣어주세요 language = 개발언어, sort_tag = 'stars' or 'forks'
+language = 
+sort_tag = 
+'''
+
+res = requests.get("https://github.com/topics/{}}?o=desc&s={}".format(language, sort_tag), user_agent)
+soup = BeautifulSoup(res.text, "html.parser")
+
+# repository의 타이틀을 가져와 각각의 페이지로 이동하기 위해 저장해줍니다..
+repositorys = soup.find_all("h3", class_=("f3 color-fg-muted text-normal lh-condensed"))
+repo_info = []
+for repository in repositorys:
+    repo_info.append(repository.text.replace('\n', '').replace(' ', ''))
+
+git_data = []
+
+for repo_list in repo_info:
+    res_repo = requests.get("https://github.com/" + repo_list , user_agent)
+    soup_repo = BeautifulSoup(res_repo.text, "html.parser")
+    
+    # repo_watch는 안긁어와지는데 해결가능할까요
+    repo_watch = soup_repo.find("span", id= "repo-notifications-counter")
+    repo_fork = soup_repo.find("span", id="repo-network-counter")
+    repo_stars = soup_repo.find("span", id="repo-stars-counter-star")
+
+    elem = {
+        "watch" : repo_watch,
+        "fork" : repo_fork,
+        "stars" : repo_stars
+    }
+    git_data.append(elem)
+    
+    
+'''
+# 수집된 데이터 파일 저장       
+with open("../data/github.pkl", "wb") as f:
+    pickle.dump(git_data, f)
+'''
+    
+'''
+# 마지막으로 JSON을 만드는 코드입니다.
+import json
+
+# 리스트를 딕셔너리로 변환
+repo_dict = dict(git_data)
+
+# 딕셔너리를 JSON으로 변환
+repo_json = json.dumps(repo_dict)
+
+# JSON 파일로 저장
+with open('repo_info.json', 'w') as json_file:
+    json_file.write(repo_json)
+'''
