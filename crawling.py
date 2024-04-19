c96f90ec14d5c615c484d1ac22c7d77783f42585
ryd11 <sdmee97@gmail.com>
2024-04-19 18:39:25 +0900

    최종 final
    진짜진짜

--- crawling.py
+++ crawling.py
@@ -8,10 +8,16 @@ language와 sort_tag를 정보에 맞게 넣어주세요 language = 개발언어
 로그인 상태를 확인해주세요 아이디와 비밀번호를 자신의 비밀번호와 아이디로 넣어주세요
 '''
 
+stack_list = []
+
 language = "개발언어"
-sort_tag = "stars or forks"
 your_id = "아이디"
 your_password = "비밀번호"
 
-git_crawling(language, sort_tag, your_id, your_password)
-stackoverflow_crawling(language)
+with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+    for i in stack_list:
+        git_crawling(i,"아이디", "비밀번호")
+
+with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+    for i in stack_list:
+        stackoverflow_crawling(i)
--- stackoverflow_crawling.py
+++ stackoverflow_crawling.py
@@ -20,7 +20,6 @@ def stackoverflow_crawling(language):
         'Spring Boot' : 'spring-boot',
         'Amazon Web Services(AWS)' : 'amazon-web-services',
         'C++' : 'c%2b%2b',
-        'CI/CD' : 'cicd',
         'MVVM(Model-View-ViewModel)' : 'mvvm'
     }
     re_sof_convert = {
@@ -38,7 +37,6 @@ def stackoverflow_crawling(language):
         'spring-boot' : 'Spring Boot',
         'amazon-web-services' : 'Amazon Web Services(AWS)',
         'c%2b%2b' : 'C++',
-        'cicd' : 'CI/CD',
         'mvvm' : 'MVVM(Model-View-ViewModel)'
     }
     
@@ -46,80 +44,65 @@ def stackoverflow_crawling(language):
     if language in sof_convert:
         language = sof_convert[language]
     
-    # assets/img/git/language 폴더가 없으면 생성
-    folder_sof = os.path.join("assets","img","sof", language)
-    if not os.path.exists(folder_sof):
-        os.makedirs(folder_sof)
+    driver.get("https://stackoverflow.com/questions/tagged/{}?tab=active&page=1&pagesize=15".format(language))
     
-    with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
-        driver.get("https://stackoverflow.com/questions/tagged/{}?tab=active&page=1&pagesize=15".format(language))
+    i = 1
+    sof_data = []
+    
+    # title 경로 찾기
+    id_list = []
+    
+    # 질문 요소들을 찾기 위한 XPath
+    id_elements = driver.find_elements(By.XPATH, '//div[contains(@class, "s-post-summary    js-post-summary")]')
+    
+    # 각 질문 요소에 대해 반복
+    for id_element in id_elements:
+        # 질문 요소의 id 속성 가져오기
+        question_id = id_element.get_attribute("id")
+        id_list.append(question_id)
+    
+    # 인덱스가 리스트의 길이보다 작을 때 반복
+    while i <= len(id_list):
+        # 요소 찾기
+        question_title = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/h3/a'.format(id_list[i-1]))
+        question_time = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/time/a/span'.format(id_list[i-1]))
+        question_writer = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/div/div/a'.format(id_list[i-1]))
+        question_img = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/a/div/img'.format(id_list[i-1]))
+        question_votes = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[1]/span[1]'.format(id_list[i-1]))
+        question_answers = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[2]/span[1]'.format(id_list[i-1]))
+        question_views = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[3]/span[1]'.format(id_list[i-1]))
+        # 이미지 소스 URL 가져오기
+        img_src = question_img.get_attribute("src")
         
-        i = 1
-        sof_data = []
+        # 요소의 title 속성값 가져와 sof_data에 저장
+        sof_elem = {
+            "title" : question_title.text,
+            "time" : question_time.text,
+            "writer" : question_writer.text,
+            "votes" : question_votes.text,
+            "answers" : question_answers.text,
+            "views" : question_views.text,
+            "url" : driver.current_url,
+            "img" : img_src,
+        }
+        sof_data.append(sof_elem)
+        i+=1
         
-        # title 경로 찾기
-        id_list = []
-        
-        # 질문 요소들을 찾기 위한 XPath
-        id_elements = driver.find_elements(By.XPATH, '//div[contains(@class, "s-post-summary    js-post-summary")]')
-        
-        # 각 질문 요소에 대해 반복
-        for id_element in id_elements:
-            # 질문 요소의 id 속성 가져오기
-            question_id = id_element.get_attribute("id")
-            id_list.append(question_id)
-        
-        # 인덱스가 리스트의 길이보다 작을 때 반복
-        while i <= len(id_list):
-            # 요소 찾기
-            question_title = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/h3/a'.format(id_list[i-1]))
-            question_time = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/time/a/span'.format(id_list[i-1]))
-            question_writer = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/div/div/a'.format(id_list[i-1]))
-            question_img = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/div[2]/div[2]/a/div/img'.format(id_list[i-1]))
-            question_votes = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[1]/span[1]'.format(id_list[i-1]))
-            question_answers = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[2]/span[1]'.format(id_list[i-1]))
-            question_views = driver.find_element(By.XPATH, '//*[@id="{}"]/div[1]/div[3]/span[1]'.format(id_list[i-1]))
-            question_url = driver.find_element(By.XPATH, '//*[@id="{}"]/div[2]/h3/a'.format(id_list[i-1]))
-            
-            
-            # 이미지 소스 URL 가져오기
-            img_src = question_img.get_attribute("src")
-            
-            # 이미지 다운로드
-            img_name = question_writer.text + ".jpg"  # 이미지 파일 이름 설정
-            # language 변수의 값이 re_sof_convert 딕셔너리의 키에 있는지 확인하고 변경
-            if language in re_sof_convert:
-                language = re_sof_convert[language]
-            img_path = os.path.join("assets", "img", "sof", language, img_name)  # 이미지 파일 경로 설정
-            
-            # 이미지 다운로드 및 저장
-            with open(img_path, "wb") as img_file:
-                img_file.write(requests.get(img_src).content)
-            
-            # 요소의 title 속성값 가져와 sof_data에 저장
-            sof_elem = {
-                "title" : question_title.text,
-                "time" : question_time.text,
-                "writer" : question_writer.text,
-                "votes" : question_votes.text,
-                "answers" : question_answers.text,
-                "views" : question_views.text,
-                "url" : question_url.get_attribute('href'),
-            }
-            sof_data.append(sof_elem)
-            i+=1
+    # 이름 재변환
+    if language in re_sof_convert:
+        language = re_sof_convert[language]
 
     # assets/data/sof 폴더가 없으면 생성
     folder_git = os.path.join("assets","data","sof")
     if not os.path.exists(folder_git):
         os.makedirs(folder_git)
-
+        
     # CSV 파일로 저장
     csv_file_path = os.path.join("assets", "data", "sof", "sof_info_{}.csv".format(language))
-
+    
     # CSV 파일로 저장
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=["title", "time", "writer","votes","answers","views","url"])
+        writer = csv.DictWriter(csv_file, fieldnames=["title", "time", "writer","votes","answers","views","url", "img"])
         writer.writeheader()
         for elem in sof_data:
             writer.writerow(elem)
